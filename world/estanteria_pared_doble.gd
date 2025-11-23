c104ab8267a082e664b4a129846d927fab223e51
NahuelBigu <nahuel_bigu@hotmail.com>
2025-11-23 02:15:39 -0300

    New products, new order

--- world/estanteria_pared_doble.gd
+++ world/estanteria_pared_doble.gd
@@ -21,22 +21,24 @@ func spawnear_objetos_random():
 		return
 
 	# Obtenemos todos los hijos del contenedor (tus Markers)
-	var puntos_spawn = contenedor_puntos.get_children()
-	
-	# --- OPCIÓN A: Poner un objeto random en CADA punto ---
-	for punto in puntos_spawn:
-		generar_instancia(punto)
+	var estanterias = contenedor_puntos.get_children()
+	for estatante in estanterias:
+		var puntos_spawn = estatante.get_children()
+		# 1. Elegimos una escena al azar del array
+		var escena_random = objetos_posibles.pick_random()
+		# --- OPCIÓN A: Poner un objeto random en CADA punto ---
+		for punto in puntos_spawn:
+			generar_instancia(punto,escena_random)
 
 	# --- OPCIÓN B: (Alternativa) Elegir SOLO UN punto al azar ---
 	# var punto_random = puntos_spawn.pick_random()
 	# generar_instancia(punto_random)
 
-func generar_instancia(punto_objetivo):
-	# 1. Elegimos una escena al azar del array
-	var escena_random = objetos_posibles.pick_random()
+func generar_instancia(punto_objetivo,escena_random):
 	
 	# 2. Instanciamos (creamos) el objeto
 	var nuevo_objeto = escena_random.instantiate()
+	Globals.productos.append(nuevo_objeto)
 	add_child(nuevo_objeto)
 
 	# ESTA es la línea mágica:
